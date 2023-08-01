07c5041493d90483f8bafe244fa1e1a99105c457
kstutts <>
2023-08-01 14:39:11 -0700

    added basic nav and a few screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,37 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import HomeScreen from '../src/components/HomeScreen';
-import ProfileScreen from '../src/components/ProfileScreen';
+import HomeScreen from './scenes/home/Home';
+import ProfileScreen from './scenes/profile/Profile';
+import DashboardScreen from './scenes/dashboard/Dashboard';
 
 type RootStackParamList = {
-  Home: undefined;
-  Profile: {name: string; route: string};
-  Feed: {sort: 'latest' | 'top'} | undefined;
+    Home: undefined;
+    Profile: undefined;
+    Dashboard: undefined;
 };
-
 const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 const MyStack = () => {
-  return (
-    <RootStack.Navigator>
-      <RootStack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{title: 'Welcome'}}
-      />
-      <RootStack.Screen name="Profile" component={ProfileScreen} />
-    </RootStack.Navigator>
-  );
+    return (
+        <RootStack.Navigator>
+            <RootStack.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{title: 'ayib'}}
+            />
+            <RootStack.Screen name="Profile" component={ProfileScreen} />
+            <RootStack.Screen name="Dashboard" component={DashboardScreen} />
+        </RootStack.Navigator>
+    );
 };
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <MyStack />
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <MyStack />
+        </NavigationContainer>
+    );
 };
 
 export default App;
--- src/scenes/profile/Profile.tsx
+++ src/scenes/profile/Profile.tsx
@@ -5,13 +5,12 @@ import {Text} from 'react-native';
 type RootStackParamList = {
     Home: undefined;
     Profile: {name: string; route: string};
-    Feed: {sort: 'latest' | 'top'} | undefined;
 };
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Profile'>;
 
-const ProfileScreen = ({navigation, route}: Props) => {
-    return <Text>This is {route.params.name}'s profile</Text>;
+const ProfileScreen = () => {
+    return <Text>This is profile</Text>;
 };
 
 export default ProfileScreen;
